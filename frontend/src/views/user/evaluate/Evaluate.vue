98ee619e0a3492c8fe429185fe1da8616c66b883
Fankekeke <1258846421@qq.com>
2023-09-06 22:39:56 +0800

    fix- 车辆维修管理

--- frontend/src/views/user/evaluate/Evaluate.vue
+++ frontend/src/views/user/evaluate/Evaluate.vue
@@ -321,7 +321,7 @@ export default {
       if (params.type === undefined) {
         delete params.type
       }
-      params.userId = this.currentUser.usesrId
+      params.userId = this.currentUser.userId
       this.$get('/cos/order-evaluate/page', {
         ...params
       }).then((r) => {
--- frontend/src/views/user/order/OrderView.vue
+++ frontend/src/views/user/order/OrderView.vue
@@ -1,109 +1,139 @@
 <template>
-  <a-modal v-model="show" title="订单详情" @cancel="onClose" :width="900">
+  <a-modal v-model="show" title="订单详情" @cancel="onClose" :width="1200">
     <template slot="footer">
       <a-button key="back" @click="onClose" type="danger">
         关闭
       </a-button>
     </template>
-    <div style="font-size: 13px;font-family: SimHei" v-if="orderData !== null">
+    <div style="font-size: 13px;font-family: SimHei" v-if="orderInfo !== null">
       <a-row style="padding-left: 24px;padding-right: 24px;">
-        <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">基础信息</span></a-col>
-        <a-col :span="8"><b>订单编号：</b>
-          {{ orderData.code }}
+        <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">订单信息</span></a-col>
+        <a-col :span="6"><b>订单编号：</b>
+          {{ orderInfo.code }}
         </a-col>
-        <a-col :span="8"><b>订单名称：</b>
-          {{ orderData.orderName ? orderData.orderName : '- -' }}
+        <a-col :span="6"><b>订单名称：</b>
+          {{ orderInfo.orderName ? orderInfo.orderName : '- -' }}
         </a-col>
-        <a-col :span="8"><b>价格/日：</b>
-          {{ orderData.dayPrice ? orderData.dayPrice + '元' : '- -' }}
+        <a-col :span="6"><b>车辆每日租金：</b>
+          {{ orderInfo.dayPrice ? orderInfo.dayPrice : '- -' }} 元
+        </a-col>
+        <a-col :span="6"><b>总价格：</b>
+          {{ orderInfo.total }} 元
         </a-col>
       </a-row>
       <br/>
       <a-row style="padding-left: 24px;padding-right: 24px;">
-        <a-col :span="8"><b>开始租车时间：</b>
-          {{ orderData.startDate }}
+        <a-col :span="6"><b>租车天数：</b>
+          {{ orderInfo.rentDay }} 天
         </a-col>
-        <a-col :span="8"><b>归还车辆时间：</b>
-          {{ orderData.endDate ? orderData.endDate : '- -' }}
+        <a-col :span="6"><b>开始租车时间：</b>
+          {{ orderInfo.startDate ? orderInfo.startDate : '- -' }}
         </a-col>
-        <a-col :span="8"><b>总价格：</b>
-          {{ orderData.total ? orderData.total + '元' : '- -' }}
+        <a-col :span="6"><b>归还车辆时间</b>
+          {{ orderInfo.endDate ? orderInfo.endDate : '- -' }}
         </a-col>
-      </a-row>
-      <br/>
-      <a-row style="padding-left: 24px;padding-right: 24px;">
-        <a-col :span="8"><b>订单状态：</b>
-          <span v-if="orderData.status == -1">未支付</span>
-          <span v-if="orderData.status == 0">未完成</span>
-          <span v-if="orderData.status == 1">已完成</span>
-        </a-col>
-        <a-col :span="8"><b>取车店铺：</b>
-          {{ orderData.takeShop ? orderData.takeShop : '- -' }}
-        </a-col>
-        <a-col :span="8"><b>归还店铺：</b>
-          {{ orderData.returnShop ? orderData.returnShop : '- -' }}
+        <a-col :span="6"><b>下单时间：</b>
+          {{ orderInfo.createDate }}
         </a-col>
       </a-row>
       <br/>
       <a-row style="padding-left: 24px;padding-right: 24px;">
-        <a-col :span="8"><b>下单时间：</b>
-          {{ orderData.createDate ? orderData.createDate : '- -' }}
+        <a-col :span="6"><b>订单状态：</b>
+          <span v-if="orderInfo.status == -1">未支付</span>
+          <span v-if="orderInfo.status == 0">未完成</span>
+          <span v-if="orderInfo.status == 1">已完成</span>
         </a-col>
-        <a-col :span="8"><b>备注：</b>
-          {{ orderData.remark ? orderData.remark : '- -' }}
+        <a-col :span="18"><b>备注：</b>
+          {{ orderInfo.remark }}
         </a-col>
       </a-row>
       <br/>
-      <a-row style="padding-left: 24px;padding-right: 24px;" v-if="vehicleInfo != null">
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col :span="12"><b>取车店铺：</b>
+          {{ orderInfo.takeShop ? orderInfo.takeShop : '- -' }}
+        </a-col>
+        <a-col :span="12"><b>归还车辆店铺：</b>
+          {{ orderInfo.returnShop ? orderInfo.returnShop : '- -' }}
+        </a-col>
+      </a-row>
+    </div>
+    <br/>
+    <br/>
+    <div style="font-size: 13px;font-family: SimHei" v-if="userInfo !== null">
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">评价用户</span></a-col>
+        <a-col :span="6"><b>用户姓名：</b>
+          {{ userInfo.name }}
+        </a-col>
+        <a-col :span="6"><b>联系电话：</b>
+          {{ userInfo.phone ? userInfo.phone : '- -' }}
+        </a-col>
+        <a-col :span="6"><b>会员编号：</b>
+          {{ userInfo.code ? userInfo.code : '- -' }}
+        </a-col>
+      </a-row>
+    </div>
+    <br/>
+    <br/>
+    <div style="font-size: 13px;font-family: SimHei" v-if="vehicleInfo !== null">
+      <a-row style="padding-left: 24px;padding-right: 24px;">
         <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">车辆信息</span></a-col>
-        <a-col :span="8"><b>车辆编号：</b>
+        <a-col :span="6"><b>车辆编号：</b>
           {{ vehicleInfo.vehicleNo }}
         </a-col>
-        <a-col :span="8"><b>车牌号：</b>
-          {{ vehicleInfo.vehicleNumber }}
+        <a-col :span="6"><b>车牌号：</b>
+          {{ vehicleInfo.vehicleNumber ? vehicleInfo.vehicleNumber : '- -' }}
         </a-col>
-        <a-col :span="8"><b>车辆颜色：</b>
-          {{ vehicleInfo.vehicleColor }}
+        <a-col :span="6"><b>车辆颜色：</b>
+          {{ vehicleInfo.vehicleColor ? vehicleInfo.vehicleColor : '- -' }}
         </a-col>
-      </a-row>
-      <br/>
-      <a-row style="padding-left: 24px;padding-right: 24px;" v-if="vehicleInfo != null">
-        <a-col :span="8"><b>车辆名称：</b>
+        <a-col :span="6"><b>车辆名称：</b>
           {{ vehicleInfo.name }}
         </a-col>
-        <a-col :span="8"><b>发动机号码：</b>
-          {{ vehicleInfo.engineNo }}
-        </a-col>
-        <a-col :span="8"><b>载客数量：</b>
+      </a-row>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col :span="6"><b>载客数量：</b>
           {{ vehicleInfo.carryPassengers }}
         </a-col>
-      </a-row>
-      <br/>
-      <a-row style="padding-left: 24px;padding-right: 24px;" v-if="vehicleInfo != null">
-        <a-col :span="8"><b>负责人：</b>
+        <a-col :span="6"><b>发动机号码：</b>
+          {{ vehicleInfo.engineNo }}
+        </a-col>
+        <a-col :span="6"><b>车辆状态：</b>
+          <span v-if="vehicleInfo.status == 0" style="color: red">空闲</span>
+          <span v-if="vehicleInfo.status == 1" style="color: green">使用中</span>
+          <span v-if="vehicleInfo.status == 2" style="color: green">维修中</span>
+          <span v-if="vehicleInfo.status == 3" style="color: green">已报废</span>
+        </a-col>
+        <a-col :span="6"><b>负责人：</b>
           {{ vehicleInfo.principal }}
         </a-col>
-        <a-col :span="8"><b>联系电话：</b>
+      </a-row>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col :span="6"><b>联系电话：</b>
           {{ vehicleInfo.phone }}
         </a-col>
-        <a-col :span="8"><b>出场日期：</b>
+        <a-col :span="6"><b>出厂日期：</b>
           {{ vehicleInfo.factoryDate }}
         </a-col>
-      </a-row>
-      <br/>
-      <a-row style="padding-left: 24px;padding-right: 24px;" v-if="vehicleInfo != null">
-        <a-col :span="8"><b>排放标准：</b>
+        <a-col :span="6"><b>排放标准：</b>
           {{ vehicleInfo.emissionStandard }}
         </a-col>
-        <a-col :span="8"><b>燃料类型：</b>
-          <span v-if="vehicleInfo.fuelType == 1">燃油</span>
-          <span v-if="vehicleInfo.fuelType == 2">柴油</span>
-          <span v-if="vehicleInfo.fuelType == 3">油电混动</span>
-          <span v-if="vehicleInfo.fuelType == 4">电能</span>
+        <a-col :span="6"><b>燃料类型：</b>
+          <span v-if="vehicleInfo.fuelType == 1" style="color: green">燃油</span>
+          <span v-if="vehicleInfo.fuelType == 2" style="color: green">柴油</span>
+          <span v-if="vehicleInfo.fuelType == 3" style="color: green">油电混动</span>
+          <span v-if="vehicleInfo.fuelType == 4" style="color: green">电能</span>
+        </a-col>
+        <br/>
+        <br/>
+        <a-col :span="24"><b>备注：</b>
+          {{ vehicleInfo.content }}
         </a-col>
       </a-row>
       <br/>
-      <a-row style="padding-left: 24px;padding-right: 24px;" v-if="vehicleInfo != null">
+      <a-row style="padding-left: 24px;padding-right: 24px;">
         <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">图册</span></a-col>
         <a-col :span="24">
           <a-upload
@@ -121,18 +151,6 @@
         </a-col>
       </a-row>
       <br/>
-      <a-row style="padding-left: 24px;padding-right: 24px;" v-if="userInfo !== null">
-        <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">用户信息</span></a-col>
-        <a-col :span="8"><b>客户编号：</b>
-          {{ userInfo.code }}
-        </a-col>
-        <a-col :span="8"><b>客户名称：</b>
-          {{ userInfo.name }}
-        </a-col>
-        <a-col :span="8"><b>联系方式：</b>
-          {{ userInfo.phone }}
-        </a-col>
-      </a-row>
     </div>
   </a-modal>
 </template>
@@ -187,17 +205,18 @@ export default {
   watch: {
     orderShow: function (value) {
       if (value) {
-        this.dataInit(this.orderData.code)
+        this.dataInit(this.orderData.id)
+
       }
     }
   },
   methods: {
-    dataInit (orderCode) {
-      this.$get(`/cos/vehicle-info/order/detail/${orderCode}`).then((r) => {
+    dataInit (orderId) {
+      this.$get(`/cos/vehicle-info/order/detail/${orderId}`).then((r) => {
         this.userInfo = r.data.user
-        this.vehicleInfo = r.data.vehicle
         this.orderInfo = r.data.order
-        this.imagesInit(this.vehicleInfo.images)
+        this.vehicleInfo = r.data.vehicle
+        this.imagesInit(this.orderData.images)
       })
     },
     imagesInit (images) {
